c8967bf2ddb3c67a533568ffaca9174f8f1afbb4
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-11-11 11:44:53 +0100

    Segundo ejercicio del boletin nueve

---
+++ boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,13 @@
+
+package boletin9_2;
+
+
+public class Boletin9_2 {
+
+   
+    public static void main(String[] args) {
+      Numeros num=new Numeros();
+    num.sumaYproducto();
+    }
+    
+}
---
+++ boletin9_2/src/boletin9_2/Numeros.java
@@ -0,0 +1,22 @@
+
+package boletin9_2;
+
+
+public class Numeros {
+  
+ public void sumaYproducto(){
+     int acusum=0;
+      double acuprod=1;
+     for(int c=10;c<=90;c++){
+     acusum=acusum+c;
+     acuprod=acuprod*c;
+     
+     }
+     
+     System.out.println("Suma de 10 a 90: "+acusum+ " productos: "+acuprod);
+  
+   
+ 
+    
+}
+}
