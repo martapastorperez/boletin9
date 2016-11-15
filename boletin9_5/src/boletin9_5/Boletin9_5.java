47250a263798eac0dda503d7adc499d8a3a45fd3
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-11-15 09:27:17 +0100

    quinto ejercicio del boletin nueve

---
+++ boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,13 @@
+
+package boletin9_5;
+
+public class Boletin9_5 {
+
+    public static void main(String[] args) {
+    Serie ser=new Serie();
+    ser.serie1();
+    ser.serie2();
+    ser.serie3();
+    }
+    
+}
